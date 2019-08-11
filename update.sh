b2c2a1c0898524823396155fb0797a17832972f8
Marco Baldanaza <marcobaldanza@hotmail.co.uk>
2019-08-11 23:04:46 +0100

    added pushd

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+pushd argv[0]
+
 source ../venv/bin/actviate
 
 python manage.py makemigrations
 python manage.py makemigrations impex
 python manage.py migrate --noinput
 zappa update
+
+popd
