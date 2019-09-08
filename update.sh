e34052e46918c533d06bf0a60ef31b6dafe5e1e4
Marco Baldanaza <marcobaldanza@hotmail.co.uk>
2019-09-08 22:25:51 +0100

    updated packages and updates.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 pushd $1
 
 source ../venv/bin/activate
-
+pip install - r requirements.txt
 python manage.py makemigrations
 python manage.py makemigrations impex
 python manage.py migrate --noinput
