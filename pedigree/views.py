c07e2d37da3397deab64033adfd7f17451a9ccb5
masystems <marco@masys.co.uk>
2019-09-23 09:58:26 +0100

    keep leeading chars in reg number suggestion

--- pedigree/views.py
+++ pedigree/views.py
@@ -353,7 +353,7 @@ def new_pedigree_form(request):
         latest_added = Pedigree.objects.filter(account=attached_service).latest('reg_no')
         latest_reg = latest_added.reg_no
         reg_ints_re = re.search("[0-9]+", latest_reg)
-        suggested_reg = latest_reg.replace(str(reg_ints_re.group(0)), str(int(reg_ints_re.group(0))+1))
+        suggested_reg = latest_reg.replace(str(reg_ints_re.group(0)), str(int(reg_ints_re.group(0))+1).zfill(len(reg_ints_re.group(0))))
     except Pedigree.DoesNotExist:
         suggested_reg = 'REG12345'
 
