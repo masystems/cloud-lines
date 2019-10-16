4385ab5443cfaca585b7c357bbefefc11cb48988
masystems <marco@masys.co.uk>
2019-10-16 22:16:29 +0100

    updated look and feel of exporting page

--- pedigree/models.py
+++ pedigree/models.py
@@ -9,14 +9,14 @@ class Pedigree(models.Model):
     class Meta:
         get_latest_by = "order_date"
 
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
-    account = models.ForeignKey(AttachedService, on_delete=models.SET_NULL, blank=True, null=True)
-    breeder = models.ForeignKey(Breeder, on_delete=models.SET_NULL, blank=True, null=True, help_text="Often the same as Current Owner")
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Creator")
+    account = models.ForeignKey(AttachedService, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Account")
+    breeder = models.ForeignKey(Breeder, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Breeder", help_text="Often the same as Current Owner")
     current_owner = models.ForeignKey(Breeder, on_delete=models.SET_NULL, blank=True, null=True, related_name='owner', verbose_name='current owner', help_text="Often the same as Breeder")
     reg_no = models.CharField(max_length=100, blank=True, unique=True, verbose_name='registration number', help_text="Must be unique")
     tag_no = models.CharField(max_length=100, blank=True, null=True, verbose_name='tag number')
-    name = models.CharField(max_length=100, blank=True)
-    description = models.TextField(max_length=1000, blank=True, null=True, help_text="Max 1000 characters")
+    name = models.CharField(max_length=100, blank=True, verbose_name="Name")
+    description = models.TextField(max_length=1000, blank=True, null=True, verbose_name="Description", help_text="Max 1000 characters")
     date_of_registration = models.DateField(blank=True, null=True, verbose_name='date of registration', help_text="Date formats: 1984/09/31, 84/09/31, 31/09/1984, 31/09/84, 1984-09-31, 84-09-31, 31-09-1984, 31-09-84")
     dob = models.DateField(blank=True, null=True, verbose_name='date of birth', help_text="Date formats: 1984/09/31, 84/09/31, 31/09/1984, 31/09/84, 1984-09-31, 84-09-31, 31-09-1984, 31-09-84")
     dod = models.DateField(blank=True, null=True, verbose_name='date of death', help_text="Date formats: 1984/09/31, 84/09/31, 31/09/1984, 31/09/84, 1984-09-31, 84-09-31, 31-09-1984, 31-09-84")
@@ -41,7 +41,7 @@ class Pedigree(models.Model):
     parent_father_notes = models.CharField(max_length=500, blank=True, null=True, verbose_name='Father Notes', help_text="Max 500 characters")
     parent_mother = models.ForeignKey('self', related_name='mother', on_delete=models.SET_NULL, blank=True, null=True, verbose_name='mother', help_text="This should be the parents registration number.")
     parent_mother_notes = models.CharField(max_length=500, blank=True, null=True, verbose_name='Mother Notes', help_text="Max 500 characters")
-    breed_group = models.CharField(max_length=255, blank=True, null=True, verbose_name='breed group name')
+    breed_group = models.CharField(max_length=255, blank=True, null=True, verbose_name='Breed group name')
 
     # hidden
     date_added = models.DateTimeField(auto_now_add=True)
@@ -67,7 +67,7 @@ class PedigreeImage(models.Model):
 
 class PedigreeAttributes(models.Model):
     reg_no = models.OneToOneField(Pedigree, on_delete=models.CASCADE, primary_key=True, related_name='attribute')
-    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, blank=True, null=True, related_name='breed', verbose_name='breed')
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, blank=True, null=True, related_name='breed')
     custom_fields = models.TextField(blank=True)
 
     def __str__(self):
