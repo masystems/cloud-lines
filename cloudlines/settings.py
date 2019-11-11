58c51785208770f900e9634d5a7d3917dfbca08b
masystems <marco@masys.co.uk>
2019-11-11 14:05:06 +0000

    added pagination to the api

--- cloudlines/settings.py
+++ cloudlines/settings.py
@@ -93,7 +93,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 # Internationalization
