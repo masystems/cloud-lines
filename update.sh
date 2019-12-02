0bac0277fa74c2359f76409c019eeb7405c3d371
masystems <marco@masys.co.uk>
2019-12-02 15:49:11 +0000

    remove all packages before installing

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 pushd $1
 
 source ../venv/bin/activate
-
+pip freeze | xargs pip uninstall -y
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py makemigrations impex
