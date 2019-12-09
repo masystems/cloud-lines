af894338640be75c33a9940941dca6979f013d1f
masystems <marco@masys.co.uk>
2019-12-09 13:41:05 +0000

    fixed table and initial breed mk vals

--- breed/models.py
+++ breed/models.py
@@ -14,5 +14,9 @@ class Breed(models.Model):
     custom_fields = models.TextField(blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    mk_a = models.DecimalField(default=0.000, max_digits=5, decimal_places=4, verbose_name='Mean Kinship Group A')
+    mk_b = models.DecimalField(default=0.010, max_digits=5, decimal_places=4, verbose_name='Mean Kinship Group B')
+    mk_c = models.DecimalField(default=0.020, max_digits=5, decimal_places=4, verbose_name='Mean Kinship Group C')
+
     def __str__(self):
         return self.breed_name
--- pedigree/views.py
+++ pedigree/views.py
@@ -28,7 +28,7 @@ import dateutil.parser
 @login_required(login_url="/account/login")
 def search(request):
     attached_service = get_main_account(request.user)
-    pedigrees = Pedigree.objects.filter(Q(account=attached_service) | Q(account=attached_service)).exclude(state='unapproved').values('id', 'reg_no', 'mean_kinship', 'name', 'dob', 'status', 'breed', 'sex')
+    pedigrees = Pedigree.objects.filter(Q(account=attached_service) | Q(account=attached_service)).exclude(state='unapproved').values('id', 'reg_no', 'mean_kinship', 'name', 'dob', 'status', 'breed', 'breed__breed_name', 'sex')
     return render(request, 'search.html', {'pedigrees': pedigrees})
 
 
@@ -239,9 +239,9 @@ def search_results(request):
 
         # lvl 1
         try:
-            results = Pedigree.objects.filter(Q(account=attached_service,
+            pedigrees = Pedigree.objects.filter(Q(account=attached_service,
                                                 reg_no__icontains=search_string.upper()) | Q(account=attached_service,
-                                                                      name__icontains=search_string)).exclude(state='unapproved').values('id', 'reg_no', 'tag_no', 'name', 'dob', 'status', 'breed', 'sex')
+                                                                      name__icontains=search_string)).exclude(state='unapproved').values('id', 'reg_no', 'mean_kinship', 'name', 'dob', 'status', 'breed', 'breed__breed_name', 'sex')
         except ObjectDoesNotExist:
             breeders = Breeder.objects
             error = "No pedigrees found using: "
@@ -249,9 +249,9 @@ def search_results(request):
                                                    'error': error,
                                                    'search_string': search_string})
 
-        if len(results) > 1:
+        if len(pedigrees) > 1:
             return render(request, 'multiple_results.html', {'search_string': search_string,
-                                                             'results': results})
+                                                             'pedigrees': pedigrees})
         else:
             try:
                 lvl1 = Pedigree.objects.exclude(state='unapproved').get(Q(account=attached_service,
