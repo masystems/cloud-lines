3c17e545bdf6fce9b23a31ee5187a0ebc3a2b00f
masystems <marco@masys.co.uk>
2020-04-15 00:02:43 +0100

    removed creating migration commands

--- update.sh
+++ update.sh
@@ -5,9 +5,6 @@ pushd $1
 source ../venv/bin/activate
 pip freeze | xargs pip uninstall -y
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py makemigrations impex
-python manage.py makemigrations metrics
 python manage.py migrate --noinput
 zappa update
 deactivate
