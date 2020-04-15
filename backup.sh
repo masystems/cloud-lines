2d2cdfa273330c520f5dc7ad81fa2e53f26c54ba
masystems <marco@masys.co.uk>
2020-04-15 18:00:38 +0100

    delete files older than 30 days

--- backup.sh
+++ backup.sh
@@ -6,5 +6,6 @@ now=$(date +"%Y%m%d")
 source ../venv/bin/activate
 mkdir -p ../backups/
 python manage.py dumpdata --exclude auth.permission --exclude contenttypes > ../backups/${now}.json
+find /var/log -name "*.json" -type f -mtime +30 -exec rm -f {} \;
 deactivate
 popd
