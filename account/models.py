3da94ff979f6a390c93c3cf03a14c6e42615c710
masystems <marco@masys.co.uk>
2020-05-27 07:44:16 +0100

    made reg no field unique

--- account/models.py
+++ account/models.py
@@ -42,7 +42,6 @@ class AttachedService(models.Model):
     active = models.BooleanField(default=False)
     subscription_id = models.CharField(max_length=250, blank=True)
 
-
     install_available = models.BooleanField(default=False)
 
     def __str__(self):
--- pedigree/admin.py
+++ pedigree/admin.py
@@ -10,7 +10,7 @@ class PedigreeAdmin(admin.ModelAdmin):
     list_display = ('account', 'creator', 'reg_no', 'name', 'breeder')
     list_display_links = ['account']
     list_filter = ('account', 'creator', 'date_of_registration', 'breeder', 'current_owner', 'date_added')
-    search_fields = ['account', 'creator', 'reg_no', 'name', 'breeder', 'current_owner', 'date_added']
+    search_fields = ['account__user__user__username', 'creator__username', 'reg_no', 'name', 'breeder__breeding_prefix', 'current_owner__breeding_prefix', 'date_added']
     ordering = ['account']
     empty_value_display = '-empty-'
 
