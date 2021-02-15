5ed809bae983e751f9fedf1dd1a7535fc0eb8de2
masystems <marco@masys.co.uk>
2021-02-15 08:12:36 +0000

    removed pip uninstall/install

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@
 pushd $1
 
 source ../venv/bin/activate
-pip freeze | xargs pip uninstall -y
-pip install -r requirements.txt
+#pip freeze | xargs pip uninstall -y
+#pip install -r requirements.txt
 python manage.py migrate --noinput
 zappa update
 deactivate
