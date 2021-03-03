70f63ff2239e770f81f5ae74eb17ab6b5791cf4e
masystems <marco@masys.co.uk>
2021-03-03 07:40:53 +0000

    added logging

--- metrics/views.py
+++ metrics/views.py
@@ -133,7 +133,7 @@ def kinship(request):
                                                                          'sex',
                                                                          'breed__breed_name',
                                                                          'status')
-    #data = list(pedigrees)
+
     mother = Pedigree.objects.get(reg_no=request.POST['mother'])
     father = Pedigree.objects.get(reg_no=request.POST['father'])
 
@@ -209,7 +209,7 @@ async def mean_kinship(request):
 
             coi_raw = requests.post('http://metrics.cloud-lines.com/api/metrics/mean_kinship/',
                                     json=dumps(data, cls=DjangoJSONEncoder), stream=True)
-
+            logger.error(coi_raw.text)
             coi_dict = loads(coi_raw.json())
             for pedigree, value in coi_dict.items():
                 Pedigree.objects.filter(account=attached_service, id=pedigree.strip('X')).update(mean_kinship=value['1'])
