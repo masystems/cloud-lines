d130c9652a618b259754c15cdc98ed579852efd2
masystems <marco@masys.co.uk>
2021-04-23 07:41:10 +0100

    added table to display queue items

--- metrics/views.py
+++ metrics/views.py
@@ -6,12 +6,13 @@ from pedigree.models import Pedigree
 from breed.models import Breed
 from django.core.serializers.json import DjangoJSONEncoder
 from .models import CoiLastRun, MeanKinshipLastRun, StudAdvisorQueue
-from json import dumps, loads
+from json import dumps, loads, load
 from datetime import datetime, timedelta
 import logging
 import requests
 import pytz
 import boto3
+import urllib.parse
 from time import time
 from boto3.s3.transfer import TransferConfig
 from threading import Thread
@@ -54,10 +55,19 @@ def metrics(request):
         date = datetime.now()
         mean_kinship_date = date.strftime("%b %d, %Y %H:%M:%S")
 
+    # queue
+    sa_queue = StudAdvisorQueue.objects.filter(account=attached_service)
+    tld = f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/"
+    for item in sa_queue:
+        results_file = requests.get(urllib.parse.urljoin(tld, f"metrics/results-{item.file}"))
+        if results_file.status_code == 200:
+            item.complete = True
+
     return render(request, 'metrics.html', {'pedigrees': Pedigree.objects.filter(account=attached_service),
                                             'coi_date': coi_date,
                                             'mean_kinship_date': mean_kinship_date,
-                                            'breeds': Breed.objects.filter(account=attached_service)})
+                                            'breeds': Breed.objects.filter(account=attached_service),
+                                            'sa_queue': sa_queue})
 
 
 def multi_part_upload_with_s3(file_path, key_path):
@@ -211,9 +221,8 @@ def mean_kinship(request):
                 Pedigree.objects.filter(account=attached_service, id=pedigree.strip('X')).update(mean_kinship=value['1'])
 
 
-def stud_advisor_mother_details(request):
+def stud_advisor_mother_details(request, mother):
     attached_service = get_main_account(request.user)
-    mother = Pedigree.objects.get(account=attached_service, reg_no=request.POST['mother'])
     cois = Pedigree.objects.filter(account=attached_service, breed=mother.breed, status='alive').values('coi')
     total = 0
     for coi in cois.all():
@@ -230,13 +239,14 @@ def stud_advisor_mother_details(request):
 
 
 def stud_advisor(request):
-    mother_details = stud_advisor_mother_details(request)
-    mother_details = eval(mother_details.content.decode())
     attached_service = get_main_account(request.user)
     epoch = int(time())
 
     mother = request.POST['mother']
     mother = Pedigree.objects.get(account=attached_service, reg_no=mother)
+    mother_details = stud_advisor_mother_details(request, mother)
+    mother_details = eval(mother_details.content.decode())
+
     pedigrees = Pedigree.objects.filter(account=attached_service,
                                         breed=mother.breed).values('id',
                                                                    'parent_father__id',
@@ -268,7 +278,7 @@ def stud_advisor(request):
         coi_raw = requests.post('http://metrics.cloud-lines.com/api/metrics/{}/stud_advisor/'.format(mother.id),
                                 json=dumps(data, cls=DjangoJSONEncoder), stream=True, timeout=1)
     except requests.exceptions.ReadTimeout:
-        StudAdvisorQueue.objects.create(account=attached_service, user=request.user, file=file_name)
+        StudAdvisorQueue.objects.create(account=attached_service, user=request.user, mother=mother, file=file_name)
         response = {'status': 'message',
                     'msg': "Your request will be complete in a few minutes. We'll email you with the results."}
         return HttpResponse(dumps(response))
@@ -277,15 +287,34 @@ def stud_advisor(request):
                     'msg': "Your connection timed out. Please try again or contact support if it continues."}
         return HttpResponse(dumps(response))
 
+    # this can be removed soon
     studs_raw = loads(coi_raw.json())
-    studs_data = calculate_sa_thresholds(studs_raw)
+    studs_data = calculate_sa_thresholds(studs_raw, attached_service, mother, mother_details)
+
     return HttpResponse(dumps(studs_data))
 
 
-def calculate_sa_thresholds(studs_raw):
-    studs_data = {}
+def stud_advisor_results(request, id):
+    attached_service = get_main_account(request.user)
+    sa_queue_item = StudAdvisorQueue.objects.get(account=attached_service, id=id)
+    mother_details = stud_advisor_mother_details(request, sa_queue_item.mother)
+    mother_details = eval(mother_details.content.decode())
+    resource = boto3.resource('s3')
+    bucket = resource.Bucket(settings.AWS_S3_CUSTOM_DOMAIN)
+    bucket.download_file(f"metrics/results-{sa_queue_item.file}", f'/tmp/results-{sa_queue_item.file}')
 
-    for stud, kinship in studs_raw[0].items():
+    with open(f'/tmp/results-{sa_queue_item.file}') as results_file:
+        studs_raw = load(results_file)
+    studs_data = calculate_sa_thresholds(studs_raw, attached_service, sa_queue_item.mother, mother_details)
+
+    return render(request, 'sa_results.html', {'stud_data': studs_data,
+                                               'sa_queue_item': sa_queue_item,
+                                               'mother_details': mother_details})
+
+
+def calculate_sa_thresholds(studs_raw, attached_service, mother, mother_details):
+    studs_data = {}
+    for stud, kinship in studs_raw[str(mother.id)][0].items():
         try:
             male = Pedigree.objects.get(account=attached_service, id=stud, sex='male', status='alive')
             mk_minus_mk_thresh = mother.mean_kinship - mother.breed.mk_threshold
