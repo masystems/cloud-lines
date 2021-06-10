a44533e4c9c785cdf827f7a3cb5318177fd7deab
Robert Anker <robert@masys.co.uk>
2021-06-10 12:24:16 +0100

    added a while loop to ensure suggested reg isn't the reg of an existing pedigree

--- pedigree/views.py
+++ pedigree/views.py
@@ -444,6 +444,12 @@ def new_pedigree_form(request):
     except AttributeError:
         suggested_reg = 'REG123456'
 
+    # if reg taken, increment until not taken
+    if suggested_reg == 'REG123456':
+        while Pedigree.objects.filter(account=attached_service, reg_no=suggested_reg).exists():
+            reg_ints_re = re.search("[0-9]+", suggested_reg)
+            suggested_reg = suggested_reg.replace(str(reg_ints_re.group(0)), str(int(reg_ints_re.group(0))+1).zfill(len(reg_ints_re.group(0))))
+
     return render(request, 'new_pedigree_form_base.html', {'pedigree_form': pedigree_form,
                                                            'pedigrees': Pedigree.objects.filter(account=attached_service),
                                                            'breeders': Breeder.objects.filter(account=attached_service),
