8735b8287f3ec7e333887cc868b32fe91417aba1
Robert Anker <robert@masys.co.uk>
2021-06-21 14:16:59 +0100

    had a go at sorting the admin of impex out

--- impex/admin.py
+++ impex/admin.py
@@ -1,16 +1,30 @@
 from django.contrib import admin
-from .models import DatabaseUpload
+from .models import DatabaseUpload, FileSlice
 
 
 class DatabaseUploadAdmin(admin.ModelAdmin):
-    list_display = ('account', 'database', 'file_type')
+    list_display = ('account', 'created', 'user')
     list_display_links = ['account']
-    search_fields = ('account', 'database', 'file_type')
-    list_filter = ('account', 'database', 'file_type')
-    ordering = ['account']
+    search_fields = ('account', 'created', 'user')
+    list_filter = ('account', 'created', 'user')
+    ordering = ['created']
     empty_value_display = '-empty-'
 
     save_on_top = True
 
 
 admin.site.register(DatabaseUpload, DatabaseUploadAdmin)
+
+
+class FileSliceAdmin(admin.ModelAdmin):
+    list_display = ('slice_number', 'database_upload')
+    list_display_links = ['slice_number']
+    search_fields = ('slice_number', 'database_upload')
+    list_filter = ('slice_number', 'database_upload')
+    ordering = ['slice_number']
+    empty_value_display = '-empty-'
+
+    save_on_top = True
+
+
+admin.site.register(FileSlice, FileSliceAdmin)
