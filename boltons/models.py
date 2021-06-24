ea402d37ec823ba5397981cc78d14d7d642eeec8
masystems <marco@masys.co.uk>
2021-06-24 08:43:29 +0100

    initial birth notifications model

--- boltons/models.py
+++ boltons/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 from cloud_lines.models import Bolton
 from pedigree.models import Pedigree
+from account.models import AttachedService
 
 
 class BirthNotification(models.Model):
-    pedigree = models.ForeignKey(Pedigree, related_name='birth_notification', on_delete=models.SET_NULL, blank=True, null=True)
+    account = models.ForeignKey(AttachedService, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Account")
+    mother = models.ForeignKey(Pedigree, related_name='bnmother', on_delete=models.SET_NULL, blank=True, null=True)
+    father = models.ForeignKey(Pedigree, related_name='bnfather', on_delete=models.SET_NULL, blank=True, null=True)
+    SERVICE_METHODS = (
+        ('natural_service', 'Natual Service'),
+        ('embryo_implant', 'Embryo Implant'),
+        ('ai', 'Artificial Insemination'),
+    )
+
+    service_method = models.CharField(max_length=10, choices=SERVICE_METHODS, null=True, default='unknown', verbose_name="Status")
+
+    living_males = models.IntegerField(default=0, null=True)
+    living_females = models.IntegerField(default=0, null=True)
+    deceased_males = models.IntegerField(default=0, null=True)
+    deceased_females = models.IntegerField(default=0, null=True)
+
+    comments = models.TextField(max_length=1000, blank=True, null=True, verbose_name="Comments", help_text="Max 1000 characters")
