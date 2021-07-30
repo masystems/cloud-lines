97d9389e6f3e54c96fd92f7879da48e2f04af301
Robert Anker <robert@masys.co.uk>
2021-07-30 12:54:34 +0100

    blindly coded what I think should work so that the stud advisor results show the mean kinship threshold at the time the stud advice was created

--- metrics/views.py
+++ metrics/views.py
@@ -400,7 +400,7 @@ def stud_advisor(request):
 
     coi_raw = requests.post('http://metrics.cloud-lines.com/api/metrics/stud_advisor/',
                             json=dumps(data, cls=DjangoJSONEncoder), stream=True)
-    sa = StudAdvisorQueue.objects.create(account=attached_service, user=request.user, mother=mother, file=file_name)
+    sa = StudAdvisorQueue.objects.create(account=attached_service, user=request.user, mother=mother, file=file_name, mk_threshold=mother.breed.mk_threshold)
     response = {'status': 'message',
                 'msg': "",
                 'item_id': sa.id
@@ -413,7 +413,7 @@ def stud_advisor_results(request, id):
     sa_queue_item = StudAdvisorQueue.objects.get(account=attached_service, id=id)
     mother_details = stud_advisor_mother_details(request, sa_queue_item.mother)
     #mother_details = eval(mother_details.content.decode())
-
+    mk_threshold = sa_queue_item.threshold
 
     with urllib.request.urlopen(f"https://{settings.AWS_S3_CUSTOM_DOMAIN}/metrics/results-{sa_queue_item.file}") as results_file:
         results_raw = loads(results_file.read().decode())
@@ -422,7 +422,8 @@ def stud_advisor_results(request, id):
 
     return render(request, 'sa_results.html', {'results_raw': results_raw,
                                                'sa_queue_item': sa_queue_item,
-                                               'mother_details': mother_details})
+                                               'mother_details': mother_details,
+                                               'mk_threshold': mk_threshold})
 
 
 def results_complete(request):
