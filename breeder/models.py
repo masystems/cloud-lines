0e285c7db98bf3c2755cf67f19b475aa4729b515
Robert Anker <robert@masys.co.uk>
2021-08-17 16:40:11 +0100

    set blank=True for breeder.user because it wouldn't let me set it to null in backend

--- breeder/models.py
+++ breeder/models.py
@@ -7,7 +7,7 @@ class Breeder(models.Model):
     account = models.ForeignKey(AttachedService, on_delete=models.SET_NULL, blank=True, null=True)
     breeding_prefix = models.CharField(max_length=100, blank=False)
     contact_name = models.CharField(max_length=100, blank=True, help_text="The primary contact for the breeder/owner")
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='breeder')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='breeder')
 
     address_line_1 = models.CharField(max_length=250, blank=True)
     address_line_2 = models.CharField(max_length=250, blank=True)
