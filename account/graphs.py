c4b2459d15c93eba1638dca1373703b7407dea32
Robert Anker <robert@masys.co.uk>
2021-10-08 10:50:06 +0100

    removed selected from account graphs because that's not where that's being tracked any more

--- account/graphs.py
+++ account/graphs.py
@@ -2,7 +2,7 @@ from json import dumps
 
 def get_graphs():
     return dumps({
-        "g1": {"id": "total_line", "title": "Total Pedigrees History", "selected": False}, 
-        "g2": {"id": "total_bar", "title": "Total Pedigrees", "selected": False}, 
-        "g3": {"id": "living_bar", "title": "Total Pedigrees Alive", "selected": False},
+        "g1": {"id": "total_line", "title": "Total Pedigrees History"},
+        "g2": {"id": "total_bar", "title": "Total Pedigrees"},
+        "g3": {"id": "living_bar", "title": "Total Pedigrees Alive"},
     })
