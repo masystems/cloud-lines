00f6f655d830b45b23756ca50e3b3c28eb3e5257
Robert Anker <robert@masys.co.uk>
2021-10-11 14:04:27 +0100

    updated json for site graphs

--- account/graphs.py
+++ account/graphs.py
@@ -2,7 +2,19 @@ from json import dumps
 
 def get_graphs():
     return dumps({
-        "g1": {"id": "total_line", "title": "Total Pedigrees History"},
-        "g2": {"id": "total_bar", "title": "Total Pedigrees"},
-        "g3": {"id": "living_bar", "title": "Total Pedigrees Alive"},
+        "g1": {
+            "id": "total_added",
+            "title": "Total Pedigrees Added",
+            "desc": "The total number of pedigrees in the database over the last ten years"
+        },
+        "g2": {
+            "id": "registered",
+            "title": "Pedigrees Registered",
+            "desc": "The number of pedigrees registered each year for the last ten years"
+        },
+        "g3": {
+            "id": "current_alive",
+            "title": "Current Pedigrees Alive",
+            "desc": "The current number of living males and females"
+        },
     })
