5f6ce910ec6cd64ccfc7f78bac5c9d1359b1c033
Marco Baldanza <marco@masys.co.uk>
2021-10-26 19:01:58 +0100

    made group name unique

--- breed_group/models.py
+++ breed_group/models.py
@@ -17,7 +17,7 @@ class BreedGroup(models.Model):
     account = models.ForeignKey(AttachedService, on_delete=models.SET_NULL, blank=True, null=True)
     breeder = models.ForeignKey(Breeder, on_delete=models.CASCADE, blank=True, null=True)
     breed = models.ForeignKey(Breed, on_delete=models.CASCADE, blank=True, null=True)
-    group_name = models.CharField(max_length=100)
+    group_name = models.CharField(max_length=100, unique=True)
     group_members = models.ManyToManyField(Pedigree)
     date_added = models.DateTimeField(auto_now_add=True, blank=True)
 
