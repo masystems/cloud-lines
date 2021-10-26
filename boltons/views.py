65954afd136901ef807f53f2f247a56baf5e7a0e
Marco Baldanza <marco@masys.co.uk>
2021-10-26 18:07:58 +0100

    initial modals for bolton

--- boltons/views.py
+++ boltons/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 from account.views import is_editor, get_main_account
-
+import requests
 
 # Create your views here.
 class MembershipBase(LoginRequiredMixin, TemplateView):
@@ -22,3 +22,27 @@ class Membership(MembershipBase):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+def change_bolton_state(request, bolton_id, state):
+    print(bolton_id)
+    print(state)
+    # security checks
+    # ensure user is superadmin or owner
+    # ensure account is != small tier
+
+    # get bolton from API
+    #bolton = requests.get(settings.BOLTON_URL)
+    #if state == 'enable' and :
+
+    # if activation request
+    # ensure bolton is not already active
+    # check valid payment option exists
+    # set up subscription in stripe
+    # enable bolt on
+
+    # if deactivation request
+    # ensure bolton is deactivated
+    # cancel subscription in strip
+    # disable bolton
+    return redirect('settings')
