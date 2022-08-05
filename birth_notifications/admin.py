8825163daf67b106eaf54a996cf21e3708c454c5
Marco Baldanza <marco@masys.co.uk>
2022-08-05 23:40:38 +0100

    added account creation

--- birth_notifications/admin.py
+++ birth_notifications/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import BirthNotification, BnChild
+from .models import BirthNotification, BnChild, BnStripeAccount
 
 
 admin.site.register(BirthNotification)
 admin.site.register(BnChild)
+admin.site.register(BnStripeAccount)
