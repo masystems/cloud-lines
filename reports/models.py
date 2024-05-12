dfe391c0da9c66a363fdb4587d74e1bf65ff5533
Marco Baldanza <marco@masys.co.uk>
2024-05-12 15:35:46 +0000

    added all animals by breeder/owner

--- reports/models.py
+++ reports/models.py
@@ -6,6 +6,7 @@ from account.models import AttachedService
 class ReportQueue(models.Model):
     account = models.ForeignKey(AttachedService, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    report_name = models.CharField(max_length=255, blank=True)
     from_date = models.DateField(null=True, blank=True)
     to_date = models.DateField(null=True, blank=True)
     file_name = models.CharField(max_length=255, blank=True)
--- reports/urls.py
+++ reports/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.reports, name='reports'),
     path('census/<str:type>', views.census, name='census'),
     path('all/<str:type>', views.all, name='all'),
+    path('all_animals_by_boo', views.all_animals_by_boo, name='animals_by_breeder'),
     path('census_results_complete', views.census_results_complete, name='census_results_complete'),
     path('fangr', views.fangr, name='fangr')
-
 ]
